940f3f0a3528798d1dd469a9ef51e4f3c5a0dcb2
NDK <kartha.n@northeastern.edu>
2018-02-08 17:46:45 -0500

    modified network teardown script

--- infrastructure/aws/cloudformation/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-networking-teardown.sh
@@ -3,3 +3,7 @@ StackName=$1
 echo "Terminating $StackName network setup"
 
 aws cloudformation delete-stack --stack-name $StackName
+
+
+echo "$StackName network terminated!!!!"
+#aws cloudformation describe-stack-events --stack-name stackapp --query 'StackEvents[?ResourceStatus==`DELETE_COMPLETE`].[ResourceType,ResourceStatus,Timestamp]'
