2cc7d8a73bdf3cb2db6ba8ea79f0f19dc4c59062
Nandeep Nelagondanahalli <nelagondanahalli.n@husky.neu.edu>
2018-02-09 22:44:05 -0500

    network teardown complete

--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -2,4 +2,17 @@
 
 stackname=$1
 
-vpcid=`aws ec2 describe-vpcs --filters "Name=tag:Name,Values='$stackname'" --query Vpcs[*].VpcId --output text`
+vpcid=`aws ec2 describe-vpcs --filters "Name=tag:Name,Values='$stackname-csye6225-vpc'" --query Vpcs[*].VpcId --output text`
+echo "Vpcid: $vpcid"
+
+internetgatewayid=`aws ec2 describe-internet-gateways --filters "Name=tag:Name,Values='$stackname-csye6225-internetGatewayId'" --query InternetGateways[*].InternetGatewayId --output text`
+aws ec2 detach-internet-gateway --internet-gateway-id $internetgatewayid --vpc-id $vpcid
+echo "Internet gateway detached"
+aws ec2 delete-internet-gateway --internet-gateway-id $internetgatewayid
+echo "Internet gateway deleted"
+
+routetableid=`aws ec2 describe-route-tables --filters "Name=tag:Name,Values='$stackname-csye6225-public-route-table'" --query RouteTables[*].RouteTableId --output text`
+aws ec2 delete-route-table --route-table-id $routetableid
+echo "Route table deleted"
+aws ec2 delete-vpc --vpc-id $vpcid
+echo "Vcp $stackname -- $vpcid deleted"
