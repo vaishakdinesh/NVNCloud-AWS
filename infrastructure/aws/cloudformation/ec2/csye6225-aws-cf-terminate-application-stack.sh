3590d751f7d3063ca89e71cea74a81d1688697a4
NDK <kartha.n@northeastern.edu>
2018-02-09 22:29:49 -0500

    adding script to terminate an EC2 instance

--- infrastructure/aws/cloudformation/ec2/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/ec2/csye6225-aws-cf-terminate-application-stack.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ec2name=$1
-instanceid=`aws ec2 describe-instances --filters 'Name=tag:Name,Values=\`'$ec2name'\`' --output text`
-echo="instance-id: $instanceid"
+instanceid=`aws ec2 describe-instances --filters "Name=tag:Name,Values='$ec2name'" --query Reservations[*].Instances[*].InstanceId --output text`
+echo "instance-id: $instanceid"
 aws ec2 modify-instance-attribute --instance-id $instanceid --block-device-mappings "[{\"DeviceName\": \"/dev/sda1\",\"Ebs\":{\"DeleteOnTermination\":true}}]"
-echo="DeleteOnTermination set to TRUE"
+echo "DeleteOnTermination set to TRUE"
 aws ec2 terminate-instances --instance-ids $instanceid
-echo="ec2 instance ($instanceid) terminated"
+
+echo "ec2 instance $ec2name($instanceid) terminated"
