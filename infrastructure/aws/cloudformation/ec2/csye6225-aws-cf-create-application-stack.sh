4635351d866c3cd3c5d4aa93ec87fb80ac3effd2
NDK <kartha.n@northeastern.edu>
2018-02-14 19:51:12 -0500

    modifying application stack json & scripts

--- infrastructure/aws/cloudformation/ec2/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/ec2/csye6225-aws-cf-create-application-stack.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-InstanceName=$1
-InstanceId=`aws ec2 run-instances --cli-input-json file://csye6225-cf-application.json --query Instances[*][InstanceId] --output text`
-aws ec2 create-tags --resources $InstanceId --tags Key=Name,Value=$InstanceName
 
-InstanceState=""
 
-until [ "$InstanceState" = "running" ]; do
-  InstanceState=`aws ec2 describe-instance-status --query 'InstanceStatuses[?InstanceId==\`'$InstanceId'\`][InstanceState][*][Name]' --output text`
-  echo $InstanceState
-  sleep 20
-done
 
-echo "$InstanceName ec2 instance is up & running!!!!"
+
+
+# Assignment 4
+# InstanceName=$1
+# InstanceId=`aws ec2 run-instances --cli-input-json file://csye6225-cf-application.json --query Instances[*][InstanceId] --output text`
+# aws ec2 create-tags --resources $InstanceId --tags Key=Name,Value=$InstanceName
+#
+# InstanceState=""
+#
+# until [ "$InstanceState" = "running" ]; do
+#   InstanceState=`aws ec2 describe-instance-status --query 'InstanceStatuses[?InstanceId==\`'$InstanceId'\`][InstanceState][*][Name]' --output text`
+#   echo $InstanceState
+#   sleep 20
+# done
+#
+# echo "$InstanceName ec2 instance is up & running!!!!"
--- infrastructure/aws/cloudformation/ec2/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/ec2/csye6225-aws-cf-terminate-application-stack.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-ec2name=$1
-instanceid=`aws ec2 describe-instances --filters "Name=tag:Name,Values='$ec2name'" --query Reservations[*].Instances[*].InstanceId --output text`
-echo "instance-id: $instanceid"
-aws ec2 modify-instance-attribute --instance-id $instanceid --block-device-mappings "[{\"DeviceName\": \"/dev/sda1\",\"Ebs\":{\"DeleteOnTermination\":true}}]"
-echo "DeleteOnTermination set to TRUE"
-aws ec2 terminate-instances --instance-ids $instanceid
 
-echo "ec2 instance $ec2name($instanceid) terminated"
+
+
+
+# Assignment 4
+# ec2name=$1
+# instanceid=`aws ec2 describe-instances --filters "Name=tag:Name,Values='$ec2name'" --query Reservations[*].Instances[*].InstanceId --output text`
+# echo "instance-id: $instanceid"
+# aws ec2 modify-instance-attribute --instance-id $instanceid --block-device-mappings "[{\"DeviceName\": \"/dev/sda1\",\"Ebs\":{\"DeleteOnTermination\":true}}]"
+# echo "DeleteOnTermination set to TRUE"
+# aws ec2 terminate-instances --instance-ids $instanceid
+#
+# echo "ec2 instance $ec2name($instanceid) terminated"
