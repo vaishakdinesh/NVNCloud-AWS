b83f0461339631b33b7e4a213355fa219ab89d28
nithin <kartha.n@husky.neu.edu>
2018-02-22 22:56:27 -0500

    Created ci-cd start script

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+StackName=$1
+stackstatus=""
+createStackStatus=""
+createFlag=true
+DomainName=$2
+
+if [ -z "$StackName" ]; then
+  echo "No stack name provided. Script exiting.."
+  exit 1
+fi
+if [ -z "$DomainName" ]; then
+  echo "No domain name provided. Script exiting.."
+  exit 1
+fi
+DomainName=s3.csye6225-spring2018-$DomainName.me
+
+echo "Starting $StackName network setup"
+
+echo "Starting to create the stack......"
+
+echo "$DomainName is my code-deploy s3 bucket....."
+
+createStackStatus=`aws cloudformation create-stack --stack-name $StackName \
+  --template-body file://csye6225-cf-ci-cd.json `
+
+if [ -z "$createStackStatus" ]; then
+  echo "Failed to create stack"
+  exit 1
+fi
+
+until [ "$stackstatus" = "CREATE_COMPLETE" ]; do
+  echo "Adding resources to the stack......"
+
+  #ADD function to check resources
+  myresources(){
+    resourceStatus=`aws cloudformation describe-stack-events --stack-name $StackName --query 'StackEvents[?(ResourceType=='$@' && ResourceStatus==\`CREATE_FAILED\`)][ResourceStatus]' --output text`
+    if [ "$resourceStatus" = "CREATE_FAILED" ]; then
+      createFlag=false
+      echo "$@ creation failed! "
+      aws cloudformation describe-stack-events --stack-name $StackName --query 'StackEvents[?(ResourceType=='$@' && ResourceStatus==`CREATE_FAILED`)]'
+      echo "deleting stack..... "
+      bash ./csye6225-aws-cf-terminate-application-stack.sh $StackName
+      break
+    fi
+  }
+
+  #myresources '`AWS::EC2::Instance`'
+
+  stackstatus=`aws cloudformation describe-stacks --stack-name $StackName --query 'Stacks[*][StackStatus]' --output text`
+  sleep 20
+done
+
+if [ "$createFlag" = true ]; then
+  echo "Stack resources created successfully"
+  aws cloudformation list-stack-resources --stack-name $StackName
+fi
+exit 0
