9758988871c2c99def60ee02266cc30b1cd3f80f
vaishak <dinesh.v@husky.neu.edu>
2018-02-23 13:31:33 -0500

    s3 service interface

---
+++ app/src/main/java/edu/neu/csye6225/cloud/service/IAmazonS3Client.java
@@ -0,0 +1,8 @@
+package edu.neu.csye6225.cloud.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IAmazonS3Client {
+	public void downloadFile(String keyName);
+	public void uploadFile(String keyName, MultipartFile file);
+}
