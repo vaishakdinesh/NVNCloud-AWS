1b79f83fb48404e141103a9e43bda4260df98e79
vaishak <dinesh.v@husky.neu.edu>
2018-02-28 15:46:59 -0500

    test only in test profile now

--- app/src/test/java/edu/neu/csye6225/cloud/HuskycloudApplicationTests.java
+++ app/src/test/java/edu/neu/csye6225/cloud/HuskycloudApplicationTests.java
@@ -1,11 +1,15 @@
 package edu.neu.csye6225.cloud;
 
+import edu.neu.csye6225.cloud.configuration.DataBaseConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
@@ -20,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class HuskycloudApplicationTests {
 
