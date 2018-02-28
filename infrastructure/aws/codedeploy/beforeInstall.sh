6159e52c9c90f9968e3d0977ac520f7b6d505ae9
vaishak <dinesh.v@husky.neu.edu>
2018-02-28 18:53:00 -0500

    hail mary

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -3,5 +3,5 @@
 # change to tomcat webapps directory.
 # this directory will be different for different tomcat versions.
 cd /var/lib/tomcat8/webapps
-sudo rm -rf *
-
+sudo rm -r ROOT
+sudo rm ROOT.war
