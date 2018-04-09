f27a9292aec58619b322fd31e3006926859c7716
nithin <kartha.n@husky.neu.edu>
2018-04-09 16:56:43 -0400

    Modified all create scripts

--- infrastructure/aws/cloudformation/autoscaling/csye6225-aws-cf-create-autoscaling-stack.sh
+++ infrastructure/aws/cloudformation/autoscaling/csye6225-aws-cf-create-autoscaling-stack.sh
@@ -18,3 +18,54 @@ fi
 echo "Starting $StackName autoscaling setup"
 
 echo "Starting to create the stack......"
+
+createStackStatus=`aws cloudformation create-stack --stack-name $StackName \
+  --template-body file://csye6225-cf-application.json \
+  --parameters ParameterKey=DBName,ParameterValue=csye6225 \
+  ParameterKey=DBUser,ParameterValue=csye6225master \
+  ParameterKey=DBPassword,ParameterValue=csye6225password \
+  ParameterKey=EC2ImageId,ParameterValue=ami-66506c1c \
+  ParameterKey=EC2InstanceType,ParameterValue=t2.micro \
+  ParameterKey=EbsDeviceName,ParameterValue=/dev/sda1 \
+  ParameterKey=EbsVolumeType,ParameterValue=gp2 \
+  ParameterKey=EbsVolumeSize,ParameterValue=16 \
+  ParameterKey=KeyPairName,ParameterValue=keypair \
+  ParameterKey=AccessKeyId,ParameterValue=$AccessKeyId \
+  ParameterKey=SecretAccessKey,ParameterValue=$SecretAccessKey \
+  ParameterKey=MySqlClientPass,ParameterValue=$MySqlClientPass`
+
+  if [ -z "$createStackStatus" ]; then
+    echo "Failed to create stack"
+    exit 1
+  fi
+
+  until [ "$stackstatus" = "CREATE_COMPLETE" ]; do
+    echo "Adding resources to the stack......"
+
+    #ADD function to check resources
+    myresources(){
+      resourceStatus=`aws cloudformation describe-stack-events --stack-name $StackName --query 'StackEvents[?(ResourceType=='$@' && ResourceStatus==\`CREATE_FAILED\`)][ResourceStatus]' --output text`
+      if [ "$resourceStatus" = "CREATE_FAILED" ]; then
+        createFlag=false
+        echo "$@ creation failed! "
+        aws cloudformation describe-stack-events --stack-name $StackName --query 'StackEvents[?(ResourceType=='$@' && ResourceStatus==`CREATE_FAILED`)]'
+        echo "deleting stack..... "
+        bash ./csye6225-aws-cf-terminate-autoscaling-stack.sh $StackName
+        break
+      fi
+    }
+
+    # myresources '`AWS::EC2::Instance`'
+    # myresources '`AWS::DynamoDB::Table`'
+    # myresources '`AWS::S3::Bucket`'
+    # myresources '`AWS::RDS::DBInstance`'
+
+    stackstatus=`aws cloudformation describe-stacks --stack-name $StackName --query 'Stacks[*][StackStatus]' --output text`
+    sleep 60
+  done
+
+  if [ "$createFlag" = true ]; then
+    echo "Stack resources created successfully"
+    aws cloudformation list-stack-resources --stack-name $StackName
+  fi
+  exit 0
